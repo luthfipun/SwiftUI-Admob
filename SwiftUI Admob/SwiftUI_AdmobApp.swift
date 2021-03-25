e1c5175a766b4062cbc05b1120aa00023ab3027b
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-03-25 09:13:47 +0700

    Initial Commit

---
+++ SwiftUI Admob/SwiftUI_AdmobApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AdmobApp.swift
+//  SwiftUI Admob
+//
+//  Created by Luthfi Abdul Azis on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AdmobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
